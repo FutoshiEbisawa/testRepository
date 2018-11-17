761431bd78bdb920b18e9956956103afda77d286
FutoshiEbisawa <futoshi.ebisawa@gmail.com>
2018-11-17 12:34:13 +0900

    Initial Commit

---
+++ BookShotv1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookShotv1
+//
+//  Created by ebisawafutoshi on 2018/11/17.
+//  Copyright © 2018 Futoshi Ebisawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
